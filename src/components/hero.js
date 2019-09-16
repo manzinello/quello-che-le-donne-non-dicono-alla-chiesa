2426ddedfe9d4f8fa1de996a8033e819650e922f
Matteo Manzinello <matteo@manzinello.com>
2019-09-16 16:22:27 +0200

    in libreria

--- src/components/hero.js
+++ src/components/hero.js
@@ -33,24 +33,30 @@ const Header = ({ siteTitle }) => (
     <p style={{ textAlign: "center", maxWidth: 440, fontSize: 25 }}>
       di <strong>Ilaria Beretta</strong>
     </p>
-    <a
-      className="button"
-      href="https://amzn.to/2UOl2DG"
-      target="_blank"
-      style={{
-        margin: 5,
-        padding: ".5rem 2.5rem",
-        color: "white",
-        fontWeight: 700,
-        background: "#AC0D42",
-        borderRadius: BORDER_RADIUS,
-        borderWidth: 0,
-        cursor: "pointer",
-        textDecoration: "none",
-      }}
-    >
-      Compra su Amazon
-    </a>
+    <strong style={{ fontSize: 22, color: "#AC0D42" }}>
+      In libreria dal 1 ottobre!
+    </strong>
+    <br />
+    {false && (
+      <a
+        className="button"
+        href="https://amzn.to/2UOl2DG"
+        target="_blank"
+        style={{
+          margin: 5,
+          padding: ".5rem 2.5rem",
+          color: "white",
+          fontWeight: 700,
+          background: "#AC0D42",
+          borderRadius: BORDER_RADIUS,
+          borderWidth: 0,
+          cursor: "pointer",
+          textDecoration: "none",
+        }}
+      >
+        Compra su Amazon
+      </a>
+    )}
     <a
       className="button"
       href="https://books.google.it/books?id=r2KmDwAAQBAJ&lpg=PP1&ots=zRXvTCQqe5&dq=quello%20che%20le%20donne%20non%20dicono%20alla%20chiesa&hl=it&pg=PP1#v=onepage&q=quello%20che%20le%20donne%20non%20dicono%20alla%20chiesa&f=false"
